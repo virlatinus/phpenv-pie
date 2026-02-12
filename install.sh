11e5f097004a16d18c372561b721414f23b955f3
Werner Garcia <virlatinus@gmail.com>
2026-02-11 20:04:42 -0600

    Removed wget prerequisite

--- install.sh
+++ install.sh
@@ -4,32 +4,24 @@
 # Pie: PHP Installer for Extensions #
 #####################################
 
-FILE="bin/phpenv-pie"
-URL="https://api.github.com/repos/php/pie/releases/latest"
-KEY=".assets[0].browser_download_url"
-
 # Function for graceful exiting
 die() {
     echo $0
     exit 1
 }
 
-complete() {
-    echo "Installation complete, you can now run 'phpenv pie'"
-    exit 0
-}
+FILE="bin/phpenv-pie"
+URL="https://api.github.com/repos/php/pie/releases/latest"
+KEY=".assets[0].browser_download_url"
+
+# Check for prerequisite commands
+command -v curl >/dev/null 2>&1 || die '"curl" is not installed'
+command -v jq >/dev/null 3>&1 || die '"jq" is not installed'
 
 echo "Downloading PIE..."
 
-# Check if file already exists and is executable
-if [ -x "${FILE}" ]; then complete; fi
-
-# Check for prerequisite commands
-command -v wget >/dev/null 2>&1 || die '"wget" is not installed'
-command -v jq >/dev/null 3>&1 || die '"jq" is not installed'
-
 # Download latest release
-wget -qc -O "${FILE}" `curl -s "${URL}"|jq -r "${KEY}"`
+curl -#Lo "${FILE}" `curl -Ls "${URL}"|jq -r "${KEY}"`
 
 # Check if download was succesful
 [ -f ${FILE} ] || die 'Error downloading "${FILE}"'
@@ -37,4 +29,7 @@ wget -qc -O "${FILE}" `curl -s "${URL}"|jq -r "${KEY}"`
 # Make file xecutable
 chmod +x "${FILE}"
 
-complete
+echo "Installation complete, you can now run 'phpenv pie'"
+
+exit 0
+
